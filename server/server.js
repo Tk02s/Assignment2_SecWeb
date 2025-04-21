e1d0658e94c40ef523292909756d5662a2b1cf0b
mme888004 <mme888004@example.com>
2025-04-21 20:53:53 +0100

    version 1.0.2

--- server/server.js
+++ server/server.js
@@ -5,15 +5,14 @@ const cors=require('cors')
 const session= require('express-session')
 const cookieParser = require('cookie-parser');
 const { errorMessage } = require('./error.js');
-const { log } = require('console')
-app.use(cookieParser());
+const {users} = require('./users.js')
 
+app.use(cookieParser());
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
-const user=[{username:'user123',password:'1111',email:'amin@gmail.com',roleid:1},]
 
 app.use(session({
     secret:'sec web assignment2',
@@ -21,6 +20,10 @@ app.use(session({
     saveUninitialized:false,
     cookie:{maxAge:60000*60}
 }))
+
+
+let user={}
+
 app.use(express.static(path.join(__dirname,'../public/style')))
 
 app.get('/',(req,res)=>{
@@ -34,23 +37,20 @@ app.get('/login',(req,res)=>{
 
 app.get('/admin',(req,res)=>{
     
-   if(((req.cookies.isAdmin=='true')||(user[0].roleid===2)) &&(req.session.user))
+   if(((req.cookies.isAdmin=='true')||(user.roleid===2)) &&(req.session.user))
     res.sendFile(path.join(__dirname,'../public/admin.html'))
    else res.status(401).send(errorMessage)
  })
 
  app.get('/account',(req,res)=>{  
     
-    if(req.session.user)
-    {   
-        res.sendFile(path.join(__dirname,'../public/account.html'));
+    if (req.session.user) {
+        if(req.session.user.roleid===2)
+           res.redirect('/admin')
+        else res.sendFile(path.join(__dirname,'../public/account.html'));
+
     }
     else res.status(401).send(errorMessage)
-    
-    
-})
-app.get('/getEmail',(req,res)=>{
-    res.json({username:req.session.user.username,email:req.session.user.email});
     
     
 })
@@ -58,8 +58,9 @@ app.get('/getEmail',(req,res)=>{
 
 app.post('/login',(req,res)=>{
     const {username,password}=req.body
-    if(user[0].username==username&&user[0].password==password){
-        req.session.user={username,password,email:user[0].email}
+     user=users.find(user=>user.username===username&&user.password===password)
+    if(user){
+        req.session.user={username:user.username,password:user.password,email:user.email,roleid:user.roleid}
         if(req.session.user){
            res.cookie('isAdmin',false,{httpOnly:false,path:'/'})
            res.redirect('/account') 
@@ -81,16 +82,22 @@ app.get('/logout',(req,res)=>{
             return res.send('logout faild')
         }
         else{
+            
+            res.clearCookie('connect.sid')
             res.clearCookie('isAdmin')
             res.redirect('/')
         } 
     })
 }) 
 
+app.get('/getEmail',(req,res)=>{
+    res.json({username:req.session.user.username,email:req.session.user.email});
+})
+
 app.post('/editEmail',(req,res)=>{
     const {email,roleid}=req.body
-    user[0].email=email
-    user[0].roleid=roleid
+    user.email=email
+    user.roleid=roleid
     req.session.user.email=email
     res.redirect('/getEmail')
     
---
+++ server/users.js
@@ -0,0 +1,17 @@
+const users=[
+        {   username:'amin',
+            password:'1111',
+            email:'amin@gmail.com',
+            roleid:1},
+        
+        {   username:'admin',
+            password:'admin',
+            email:'admin@gmail.com',
+            roleid:2},
+
+        {   username:'ayoub',
+            password:'2003',
+           email:'ayoub@gmail.com',
+          roleid:1},
+    ]
+    module.exports={users}
